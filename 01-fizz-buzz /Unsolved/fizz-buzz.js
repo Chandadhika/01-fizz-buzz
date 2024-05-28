ada39f0e447b4729ae465769e5ef4abaf60bfdad
ANarngAung <anarngaung2-17@gmail.com>
2024-05-28 21:16:56 +0630

    added script and jion to html

--- 01-fizz-buzz /Unsolved/fizz-buzz.js
+++ 01-fizz-buzz /Unsolved/fizz-buzz.js
@@ -4,4 +4,70 @@
 // If a number is evenly divisible by both 3 AND 5, print "Fizz Buzz"
 // If a number is not divisible by 3 or 5, print the number
 
-var fizzBuzz = function(arr) {};
+var fizzBuzz = function(arr){
+    for (var i = 0; i < arr.length; i++){
+        var currentNumber = arr[i];
+
+        if (currentNumber % 15 === 0){
+            console.log("Fizz Buzz");
+        } else if (currentNumber % 3 === 0){
+            console.log("Fizz");
+        } else if (currentNumber % 5 === 0){
+            console.log("Buzz");
+        } else {
+            console.log(currentNumber);
+        }
+    }
+};
+
+
+
+
+
+var expect = chai.expect;
+
+before(function() {
+  window._temp = {};
+  window._temp.log = console.log;
+  window.console.log = (function(...args) {
+    var values = [];
+
+    var log = function(args) {
+      values.push(args);
+      window._temp.log(args);
+    };
+
+    log.calledWith = function() {
+      return values;
+    };
+
+    return log;
+  })();
+});
+
+describe("fizzBuzz", function() {
+  it('takes an array and for each element prints "Fizz" for numbers evenly divisible by 3, "Buzz" for numbers evenly divisible by 5, "Fizz Buzz" for numbers evenly divisible by 3 AND 5. Otherwise it prints the number if not evenly divisible by 3 or 5.', function() {
+    var arr = [6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+    fizzBuzz(arr);
+
+    expect(console.log.calledWith()).to.eql([
+      "Fizz",
+      7,
+      8,
+      "Fizz",
+      "Buzz",
+      11,
+      "Fizz",
+      13,
+      14,
+      "Fizz Buzz"
+    ]);
+  });
+});
+
+after(function() {
+  console.log = window._temp.log;
+  delete window._temp;
+});
+
